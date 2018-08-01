b1a885b7115754b4a2d2f902335fa0afcac8bf01
Maxwell Lewis <maxsolewis@gmail.com>
2018-08-01 17:04:11 +0000

    finished functionality with the exeption of a couple bugs

--- schedule/src/components/schedule/schedule.js
+++ schedule/src/components/schedule/schedule.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
-import ScheduleCourse from './scheduleCourse';
-import Gradient from '../gradient';
-import ProgressTracker from './progressTracker';
 import { connect } from 'react-redux';
 
+import Gradient from '../gradient';
+import ScheduleCourse from './scheduleCourse';
+import ProgressTracker from './progressTracker';
+
 class Schedule extends Component {
+
     renderCourses() {
-            const data = this.props.courses;
-            
-            return data.map((course, index) => {
-                 if(course.enrolled) {
-                     return <ScheduleCourse {...course} key={index}/>;
-                 }
-            });
-        }
-    
+        const data = this.props.courses;
+
+        return data.map((course, index) => {
+            if(course.enrolled && this.amountEnrolled != 5) {
+                this.amountEnrolled++;
+                return <ScheduleCourse {...course} key={index}/>;
+            }
+        });
+    }
+
     render() {
+        this.amountEnrolled = 0;
         return (
             <div className="schedule">
                 <h1 className="schedule__title">My Schedule</h1>
                 { this.renderCourses() }
-                <ProgressTracker />
-                <Gradient />
+                <ProgressTracker amountPossible={5} amountEnrolled={this.amountEnrolled} />
+                <Gradient/>
             </div>
         );
     }
-}
+ }
 
 function mapStateToProps(state) {
     return {
         courses: state.courses
-    }
+    };
 }
 
 export default connect(mapStateToProps)(Schedule);
